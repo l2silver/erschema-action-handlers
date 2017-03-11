4ccf5fa0a53dc44f76785f1e46112d7e2d4ad409
Leigh Silverstein <leighsilverstein@Leighs-MacBook-Pro.local>
2017-03-11 17:11:59 -0500

    add .gitignore and .npmignore

--- dist/index.js
+++ dist/index.js
@@ -3,22 +3,25 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.createReducer = exports.handlers = exports.actions = undefined;
+exports.relationshipReducer = exports.relationshipHandlers = exports.relationshipActions = exports.entityReducer = exports.entityHandlers = exports.entityActions = undefined;
 
-var _actions = require('./actions');
+var _actions = require('./entity/actions');
 
 var _actions2 = _interopRequireDefault(_actions);
 
-var _handlers = require('./handlers');
+var _handlers = require('./entity/handlers');
 
 var _handlers2 = _interopRequireDefault(_handlers);
 
-var _reducer = require('./reducer');
+var _reducer = require('./entity/reducer');
 
 var _reducer2 = _interopRequireDefault(_reducer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.actions = _actions2.default;
-exports.handlers = _handlers2.default;
-exports.createReducer = _reducer2.default;
+exports.entityActions = _actions2.default;
+exports.entityHandlers = _handlers2.default;
+exports.entityReducer = _reducer2.default;
+exports.relationshipActions = _actions2.default;
+exports.relationshipHandlers = _handlers2.default;
+exports.relationshipReducer = _reducer2.default;
