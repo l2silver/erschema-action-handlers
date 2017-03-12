81eb536b3fb1cb1165904295a03e3fe38cfe2181
Leigh Silverstein <leighsilverstein@Leighs-MacBook-Pro.local>
2017-03-11 23:21:55 -0500

    wron dir

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 import entityActions from './entity/actions'
 import entityHandlers from './entity/handlers'
 import entityReducer from './entity/reducer'
-import relationshipActions from './entity/actions'
-import relationshipHandlers from './entity/handlers'
-import relationshipReducer from './entity/reducer'
+import relationshipActions from './relationship/actions'
+import relationshipHandlers from './relationship/handlers'
+import relationshipReducer from './relationship/reducer'
 
 export {
   entityActions,
